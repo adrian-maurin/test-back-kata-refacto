c7674c529a2139994ad6338093f4ecaeaf84d8be
Adrian Maurin <adrian.maurin@gmail.com>
2021-12-19 19:22:07 +0100

    add docstring and comments

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -2,6 +2,14 @@
 
 class TemplateManager
 {
+    /**
+     * get computed template using input data
+     *
+     * @param Template $tpl
+     * @param array $data input data, may contain Quote and User data
+     * @return Template computed template using input $tpl and $data
+     * @access public
+     */
     public function getTemplateComputed(Template $tpl, array $data): Template
     {
         $tpl->subject = $this->computeText($tpl->subject, $data);
@@ -10,14 +18,25 @@ class TemplateManager
         return $tpl;
     }
 
+
+
+    /**
+     * compute some text, replace tags according to input data
+     *
+     * @param string $text text to compute
+     * @param array $data input data to get replacement text from
+     * @return string computed text using input $text and $data
+     * @access private
+     */
     private function computeText(string $text, array $data): string
     {
         /*
-         * Quote
+         * QUOTE
          * [quote:*]
          */
         $quote = $data['quote'] ?? null;
         if ($quote instanceof Quote) {
+            // get site & destination info using respective repositories
             $site = SiteRepository::getInstance()->getById($quote->siteId);
             $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
 
